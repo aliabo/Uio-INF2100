7d6cf99df4c1290704a1681d4079178f9ce50184
Ali Abouaid <aliabo@uio.no>
2016-09-29 03:09:55 +0200

    Youssef sin scanner lagt ved + feilen fra del1 fikset + CompoundStat lagt ved

---
+++ Project/parser/CompoundStatm.java
@@ -0,0 +1,27 @@
+package parser;
+import main.*;
+
+class CompoundStatm extends Statment{
+	CompoundStatm(int lNum) {
+	super(lNum);
+	}
+	Statement body;
+
+	static Statement parse(Scanner s) {
+	enterParser("CompundStatm");
+	
+	CompoundStatm cs = new CompoundStatm(s.curLineNum());
+	s.skip(beginToken);
+	
+	//Når man kaller parse, skal første symbol være lest inn
+
+	// Når man returnerer fra en parse, skal første symbol etter konstruksjonen være lest.
+	while(s.curToken.kind == semicolonToken){
+	s.skip(semicolonToken);
+	cs.body = Statement.parse(s);
+	}
+
+	s.skip(endToken);
+	leaveParser("CompundStatm");
+	return cs;
+}
