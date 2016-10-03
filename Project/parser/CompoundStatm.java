3d46a7ff07f9ee9ee4357ceb00373546b2194b08
Ali Abouaid <aliabo@uio.no>
2016-10-03 05:01:25 +0200

    noe syntaks feil endret

--- Project/parser/CompoundStatm.java
+++ Project/parser/CompoundStatm.java
@@ -7,21 +7,22 @@ class CompoundStatm extends Statment{
 	}
 	Statement body;
 
-	static Statement parse(Scanner s) {
-	enterParser("CompundStatm");
+	public static Statement parse(Scanner s) {
+		enterParser("CompundStatm");
 	
-	CompoundStatm cs = new CompoundStatm(s.curLineNum());
-	s.skip(beginToken);
+		CompoundStatm cs = new CompoundStatm(s.curLineNum());
+		s.skip(beginToken);
 	
-	//Når man kaller parse, skal første symbol være lest inn
+		//Når man kaller parse, skal første symbol være lest inn
 
-	// Når man returnerer fra en parse, skal første symbol etter konstruksjonen være lest.
-	while(s.curToken.kind == semicolonToken){
-	s.skip(semicolonToken);
-	cs.body = Statement.parse(s);
+		// Når man returnerer fra en parse, skal første symbol etter konstruksjonen være lest.
+		while(s.curToken.kind == semicolonToken){
+			s.skip(semicolonToken);
+			cs.body = Statement.parse(s);
+		}
+
+		s.skip(endToken);
+		leaveParser("CompundStatm");
+		return cs;
 	}
-
-	s.skip(endToken);
-	leaveParser("CompundStatm");
-	return cs;
 }
